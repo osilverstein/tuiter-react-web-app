5f122684c517dcc05ffac3f26e987567cd86b73a
osilverstein <silversteinoliver@gmail.com>
2022-11-14 15:28:28 -0800

    fix lab

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
 import Labs from "./labs";
 import HelloWorld from "./labs/a6/hello-world";
 import Tuiter from "./tuiter";
-import {BrowserRouter} from "react-router-dom";
-import {Routes, Route} from "react-router";
+import { BrowserRouter } from "react-router-dom";
+import { Routes, Route } from "react-router";
+import Assignment6 from "./labs/a6";
+import Assignment7 from "./labs/a7";
 
 function App() {
-  return (
-    <BrowserRouter>
-   <div className="container">
-       <Routes>
-           <Route index
-                  element={<Labs/>}/>
-           <Route path="/hello"
-                  element={<HelloWorld/>}/>
-           <Route path="/tuiter"
-                  element={<Tuiter/>}/>
-       </Routes>
-   </div>
-</BrowserRouter>
-  );
+       return (
+              <BrowserRouter>
+                     <div className="container">
+                            <Routes>
+                                   <Route index
+                                          element={<Labs />} />
+                                   <Route path="/hello"
+                                          element={<HelloWorld />} />
+                                   <Route path="/tuiter"
+                                          element={<Tuiter />} />
+                                   <Route path="/a6"
+                                          element={<Assignment6 />} />
+                                   <Route path="/a7"
+                                          element={<Assignment7 />} />
+                            </Routes>
+                     </div>
+              </BrowserRouter>
+       );
 }
 export default App;
